9289b37535d0ed19f4ad3e29b81ca774541e26e3
allanmauko <allan.mauko@student.moringaschool.com>
2022-09-30 16:08:07 +0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    # POST /birds
+    def create
+        bird = Bird.create(name: params[:name], species: params[:species])
+        render json: bird, status: :created
+    end
+
+    # GET /birds
+
 end
